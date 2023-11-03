b339b17029076fd8244360928a831e07d48720b7
4orty2wo <sethsantos789@gmail.com>
2023-11-03 11:10:49 -0400

    Formatting

--- StatsComponent.js
+++ StatsComponent.js
@@ -112,7 +112,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: 'column',
-        alignItems: 'flex-start', 
+        
         justifyContent: 'flex-end',
         backgroundColor: 'transparent',
     },

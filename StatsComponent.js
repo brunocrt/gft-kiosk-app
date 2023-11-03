8c782dd91b7610755061e86a026e702ec45110ac
4orty2wo <sethsantos789@gmail.com>
2023-11-03 10:59:55 -0400

    Formatting

--- StatsComponent.js
+++ StatsComponent.js
@@ -132,6 +132,8 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'center',
         backgroundColor: 'transparent',
+        borderBottomColor: 'black',
+        borderBottomColor: 1,
     },
     headerText: {
         fontSize: 18,
